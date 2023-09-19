73e2aee82c8862a50ded04e14338d9801a533229
elifftorun <145452036+elifftorun@users.noreply.github.com>
2023-09-19 16:22:24 +0300

    Add files via upload

---
+++ ornek2/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace ornek2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtYazTemizle = new System.Windows.Forms.TextBox();
+            this.btnYaz = new System.Windows.Forms.Button();
+            this.btnTemizle = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtYazTemizle
+            // 
+            this.txtYazTemizle.Location = new System.Drawing.Point(267, 97);
+            this.txtYazTemizle.Name = "txtYazTemizle";
+            this.txtYazTemizle.Size = new System.Drawing.Size(385, 20);
+            this.txtYazTemizle.TabIndex = 0;
+            // 
+            // btnYaz
+            // 
+            this.btnYaz.Location = new System.Drawing.Point(52, 42);
+            this.btnYaz.Name = "btnYaz";
+            this.btnYaz.Size = new System.Drawing.Size(102, 55);
+            this.btnYaz.TabIndex = 1;
+            this.btnYaz.Text = "Yaz";
+            this.btnYaz.UseVisualStyleBackColor = true;
+            this.btnYaz.Click += new System.EventHandler(this.btnYaz_Click);
+            // 
+            // btnTemizle
+            // 
+            this.btnTemizle.Location = new System.Drawing.Point(52, 125);
+            this.btnTemizle.Name = "btnTemizle";
+            this.btnTemizle.Size = new System.Drawing.Size(102, 55);
+            this.btnTemizle.TabIndex = 2;
+            this.btnTemizle.Text = "Temizle";
+            this.btnTemizle.UseVisualStyleBackColor = true;
+            this.btnTemizle.Click += new System.EventHandler(this.btnTemizle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(689, 226);
+            this.Controls.Add(this.btnTemizle);
+            this.Controls.Add(this.btnYaz);
+            this.Controls.Add(this.txtYazTemizle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtYazTemizle;
+        private System.Windows.Forms.Button btnYaz;
+        private System.Windows.Forms.Button btnTemizle;
+    }
+}
+
